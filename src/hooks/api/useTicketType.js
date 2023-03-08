fc89d0d3350b244d15e7e6e5db95eed634b3f31d
Isabela <isabela_cmoreira@hotmail.com>
2023-03-08 09:38:31 -0300

    Merge branch 'main' into isa

--- src/hooks/api/useTicketType.js
+++ src/hooks/api/useTicketType.js
@@ -1,16 +1,13 @@
 import useAsync from '../useAsync';
-import useToken from '../useToken';
 
 import * as ticketTypeApi from '../../services/ticketTypeApi';
 
 export default function useTicketType() {
-  const token = useToken();
-
   const {
     loading: ticketTypeLoading,
     error: ticketTypeError,
     act: ticketType
-  } = useAsync((data) => ticketTypeApi.save(data, token), false);
+  } = useAsync(ticketTypeApi.getTickets, false);
 
   return {
     ticketTypeLoading,
