3030b0f10ed55c363d6f90f4fc7f86b278164dbc
luis <lfscruz2001@gmail.com>
2023-03-08 21:48:21 -0300

    click botoes quebrado

--- src/components/Dashboard/Rooms/index.js
+++ src/components/Dashboard/Rooms/index.js
@@ -1,20 +1,72 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 import RoomCard from './roomCard';
 
 export default function Rooms() {
+  const [isSelected, setIsSelected] = useState(false);
+  const [clickedRoom, setClickedRoom] = useState(-1);
+
+  const rooms = [
+    {
+      id: 0,
+      capacity: 3
+    },
+    {
+      id: 1,
+      capacity: 2
+    },
+    {
+      id: 2,
+      capacity: 3
+    },
+    {
+      id: 3,
+      capacity: 1
+    },
+    {
+      id: 4,
+      capacity: 2
+    },
+    {
+      id: 5,
+      capacity: 2
+    },
+    {
+      id: 6,
+      capacity: 1
+    },
+    {
+      id: 7,
+      capacity: 3
+    },
+    {
+      id: 8,
+      capacity: 1
+    },
+    {
+      id: 9,
+      capacity: 2
+    },
+    {
+      id: 10,
+      capacity: 2
+    },
+  ];
+
   return (
     <RoomsSection>
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
-      <RoomCard />
+      {rooms.map((room) => {
+        return (
+          <StyledRoomCard
+            key={room.id}
+            isSelected={isSelected}
+            setIsSelected={setIsSelected}
+            clickedRoom={clickedRoom}
+            setClickedRoom={setClickedRoom}
+            room={room}
+          />
+        );
+      })}
     </RoomsSection>
   );
 }
@@ -22,4 +74,8 @@ export default function Rooms() {
 const RoomsSection = styled.div`
   display: flex;
   flex-wrap: wrap;
+`;
+
+const StyledRoomCard = styled(RoomCard)`
+  background-color: lightblue;
 `;
